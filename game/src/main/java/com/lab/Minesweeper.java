15aa2a1b0b54688b0bd1b70b1407b64367d2ec32
korn845 <kronked7833@gmaail.com>
2025-02-18 10:56:19 +0700

    1

--- game/src/main/java/com/lab/Minesweeper.java
+++ game/src/main/java/com/lab/Minesweeper.java
@@ -5,7 +5,7 @@ import java.io.InputStream;
 
 public class Minesweeper {
     static char SAFE_CELL = '.';
-    static char MINE_CELL = 'X' ;
+    static char MINE_CELL = 'X';
     static int IS_SAFE = 0;
     static int IS_MINE = 1;
     int fieldX, fieldY;
@@ -21,8 +21,8 @@ public class Minesweeper {
         this.fieldX = fieldX;
         this.fieldY = fieldY;
         this.cells = new int[fieldX][fieldY];
-        for (int i=0; i<fieldX; i++) {
-            for (int j=0; j<fieldY; j++) {
+        for (int i = 0; i < fieldX; i++) {
+            for (int j = 0; j < fieldY; j++) {
                 cells[i][j] = IS_SAFE;
             }
         }
@@ -30,15 +30,24 @@ public class Minesweeper {
 
     void displayField() {
         // Task 1: Display the mine field to terminal
+        for (int i = 0; i < fieldX; i++) {
+            for (int j = 0; j < fieldY; j++) {
+                if(cells[fieldX][fieldY] == IS_SAFE) {
+                    
+                }
+            }
+        }
     }
+
     void setMineCell(int x, int y) {
         cells[x][y] = IS_MINE;
     }
 
     void initFromFile(String mineFieldFile) {
         InputStream is = getClass().getClassLoader().getResourceAsStream(mineFieldFile);
-        
-        // Task 2: Using `java.util.Scanner` to load mine field from the input stream named, `is`
+
+        // Task 2: Using `java.util.Scanner` to load mine field from the input stream
+        // named, `is`
 
     }
 }
